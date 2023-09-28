37e9c1f30ad589ca0c420c10053f023e5272247a
Simon Egerton <SEgerton@ltu.edu.au>
2023-09-28 16:43:37 +1000

    updated crpgm.cmd to handle muliple devices attached to sequential ports i.e. via a multiport USB hub

--- CRE Firmata/Server/crpgm.cmd
+++ CRE Firmata/Server/crpgm.cmd
@@ -1,18 +1,26 @@
 @echo off
+
 set command="C:\Program Files (x86)\Arduino\hardware\tools\avr\bin\avrdude" -C"C:\Program Files (x86)\Arduino\hardware\tools\avr\etc\avrdude.conf"
 
-setlocal enableextensions enabledelayedexpansion
+REM setlocal enableextensions enabledelayedexpansion
 call :getargc argc %*
-if not %argc%==1 (
-    echo usage: crpgm [valid arduino nano COM port] i.e. crpgm COM10
-)
 
 if %argc%==1 (
     echo programming arduino
-    %command% -v -pm328p -carduino -P%1 -b57600 -Uflash:w:cre_firmata.hex:i
+    %command% -v -pm328p -carduino -PCOM%1 -b57600 -Uflash:w:cre_firmata.hex:i
+	goto :eof
 )
 
-endlocal
+if %argc%==2 (
+    echo programming %2 arduinos starting at COM%1
+	REM TODO: BUG FIX? setlocal / endlocal has no effect, endcom only picked up after second run ?! 28092023 
+	set /a endcom=%1+%2-1
+    for /L %%n in (%1,1,%endcom%) do %command% -v -pm328p -carduino -PCOM%%n -b57600 -Uflash:w:cre_firmata.hex:i
+	goto :eof
+)
+
+echo usage: crpgm [valid arduino nano COM port number ] i.e. crpgm 10 | [number of devices] i.e. crpgm 20 10 will program 10 devices starting from COM20
+
 goto :eof
 
 :getargc
